21aa2db69c4c26b19e29a987726e80564c99d037
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2021-07-01 16:13:36 +0900

    Add back containerd boilterplate (but as a non-core subproject)
    We would like to invite dennwc as a maintainer of this repo,
    so the status of the repo is now changed from core to non-core.
    
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- btrfs_tree_hc.go
+++ btrfs_tree_hc.go
@@ -1,3 +1,19 @@
+/*
+   Copyright The containerd Authors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 package btrfs
 
 // This code was auto-generated; DO NOT EDIT!
@@ -14,7 +30,7 @@ type devReplaceItemState int
 
 type blockGroup uint64
 
-// SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note
+// SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note (see headers.go)
 
 const (
 
--- headers.go
+++ headers.go
@@ -1,6 +1,24 @@
+/*
+   Copyright The containerd Authors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 package btrfs
 
 //go:generate go run ./internal/cmd/hgen.go -u -g -t BTRFS_ -p btrfs -cs=treeKeyType:uint32=_KEY,objectID:uint64=_OBJECTID -cp=fileType=FT_,fileExtentType=FILE_EXTENT_,devReplaceItemState=DEV_REPLACE_ITEM_STATE_,blockGroup:uint64=BLOCK_GROUP_ -o btrfs_tree_hc.go btrfs_tree.h
+//go:generate ltag -t ./license-templates btrfs_tree_hc.go
+//go:generate sed -i -e "s/SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note/SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note (see headers.go)/" btrfs_tree_hc.go
 //go:generate gofmt -l -w btrfs_tree_hc.go
 
 /*
